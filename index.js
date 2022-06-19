332c518d2c4a8ba489f068648be32ab58c2e2138
jd3545 <79057591+jd3545@users.noreply.github.com>
2022-06-19 16:44:29 -0400

    user and product apis finished
    apis for user delete, get user, get all users, & get user stats
    
    apis for product create, update, delete, get product, & get all products

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+
 
 
 dotenv.config();
@@ -14,6 +16,8 @@ mongoose.connect(process.env.MONGO_URL
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("Backend server is running!")
 });
